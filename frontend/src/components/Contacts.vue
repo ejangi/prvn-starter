a5dc37d82b1b3fe14287186e3b1a2354ee4388c0
ejangi <james@ejangi.com>
2019-08-17 13:34:47 +1000

    Added NGINX for frontend proxy.

---
+++ frontend/src/components/Contacts.vue
@@ -0,0 +1,33 @@
+<template>
+ <div id="contacts">
+  <div v-for="(contact) in contacts" :key="contact.id">
+   <p>{{contact.name}} is my {{contact.relationship}}</p>
+  </div>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      contacts: [],
+      name: [],
+      relationship: [],
+      errors: []
+    }
+  },
+  created () {
+    axios.get('/api/contacts')
+      .then(response => {
+        console.log(response)
+        this.contacts = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+        console.log(e)
+      })
+  }
+}
+</script>
